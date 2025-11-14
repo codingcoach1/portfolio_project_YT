e3e76ec0a24659d47929c4b11e85a6d2430fb6d3
coadingcoach1 <codingcoach2025@gmail.com>
2025-11-14 22:56:45 +0530

    color change

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
-// Q3: Ek string "JavaScript is Awesome" ko .slice() use karke first 10 characters print karo.
-let con='JavaScript is Awesome';
-let newcon = con.slice(0,12);
-console.log(newcon);
+let btn = document.querySelector('button')
+let body = document.querySelector('body')
 
-// Q4: User se ek naam lo aur template string ka use karke print karo:
 
-// "Hello <name>, Welcome to JavaScript!"
-
-// Q5: Ek string "I love JavaScript" me .replace() use karke "JavaScript" ko "Programming" se replace karo.
-
-// Q6: Ek string "apple,banana,mango" ko .split() use karke array me convert karo.
-
-// Q7: User se koi sentence lo aur check karo ki usme "js" word include hai ya nahi.
-
+btn.addEventListener('click',function(){
+    if(body.style.backgroundColor == 'black'){
+        body.style.backgroundColor = 'white';
+        btn.style.backgroundColor = 'black'
+        btn.style.color = 'white'
+    }
+    else{
+        body.style.backgroundColor = 'black';
+        btn.style.backgroundColor = 'white'
+        btn.style.color = 'black'
+    }
+})
